c027e0db10858b48454b3c10b25f1b878bd28fa4
shimon-home2021 <82209573+shimon-home2021@users.noreply.github.com>
2024-09-09 17:16:20 +0300

    kmeans

---
+++ kmeans_alg.py
@@ -0,0 +1,117 @@
+import tqdm
+from sklearn.cluster import KMeans
+import pandas as pd
+import os
+import numpy as np
+from PIL import Image, ImageFile
+from tqdm import tqdm
+import random
+
+def distance(p1,p2):
+    return np.sqrt(np.sum((p1-p2)**2))
+
+class KMeansEM:
+    def __init__(self, num_clusters=9):
+        self.num_clusters = num_clusters
+        self.pnts_assign = []
+
+    # Implementing E step
+    def assign_clusters(self, X, clusters):
+        num_changes = 0
+        for idx in range(X.shape[0]):
+            dist = []
+
+            curr_x = X[idx]
+            KK = clusters.shape[0]
+            for i in range(KK):
+                dis = distance(curr_x, clusters[i]['center'])
+                dist.append(dis)
+            curr_cluster = np.argmin(dist)
+            if self.pnts_assign[idx] != curr_cluster:
+                num_changes += 1
+            self.pnts_assign[idx] = curr_cluster
+            clusters[curr_cluster]['points'].append(curr_x)
+        return clusters,num_changes
+
+
+    # Implementing the M-Step
+    def update_clusters(self, X, clusters):
+        KK = clusters.shape[0]
+        for i in range(KK):
+            points = np.array(clusters[i]['points'])
+            if points.shape[0] > 0:
+                new_center = points.mean(axis=0)
+                clusters[i]['center'] = new_center
+
+                clusters[i]['points'] = []
+        return clusters
+
+    # Each row is an example
+    def fit(self, data=None, num_iters=100):
+        data_np = np.array(data)
+        N = data_np.shape[0]
+        clusters_ids = random.sample([k for k in range(N)], self.num_clusters)
+        clusters_init = data_np[clusters_ids,:]
+        clusters = [dict() for k in range(self.num_clusters)]
+        clusters = np.array(clusters)
+        self.pnts_assign = np.array([-1 for k in range(N)])
+        for k in range(self.num_clusters):
+            clusters[k]['points'] = []
+            clusters[k]['center'] = clusters_init[k]
+
+        for iter in tqdm(range(N),  desc="Kmeans fit"):
+            clusters, nun_changes = self.assign_clusters(data_np, clusters)
+            if nun_changes <= 0:
+                break
+            self.update_clusters(data_np, clusters)
+
+
+class KMeanAlg:
+    def __init__(self, csv_file:str='', label_dir=''):
+        self.annotations = pd.read_csv(csv_file)
+        self.label_dir = label_dir
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self, index):
+        index = index % len(self.annotations)
+        label_path = os.path.join(self.label_dir, self.annotations.iloc[index, 1])
+        bboxes = np.roll(np.loadtxt(fname=label_path, delimiter=" ", ndmin=2), 4, axis=1).tolist()
+        wh_boxes = []
+        for box in bboxes:
+            x, y, width, height, class_label = box
+            wh_boxes.append((width,height))
+        return wh_boxes
+
+    def get_dataset(self):
+        L = len(self)
+        data = []
+        for k in tqdm(range(L), desc="get_dataset"):
+            boxes = self[k]
+            data.extend(boxes)
+        return data
+
+    def get_kmean(self, K=9):
+        data = self.get_dataset()
+        #kmeans = KMeans(n_clusters=K)
+        kmeans = KMeansEM(num_clusters=9)
+        kmeans.fit(data)
+        boxes = kmeans.cluster_centers_
+        boxes = boxes[boxes[:, 1].argsort()]
+        print(boxes)
+        return boxes
+
+import argparse
+def parse_args():
+    ap = argparse.ArgumentParser("Kmeans for aspect ratios")
+    ap.add_argument('--annotation_file', type=str,required=True, help="Full path of annotation file")
+    ap.add_argument('--labels_dir', type=str, required=True, help="Full path of labels dir")
+    ap.add_argument('--k_ratios', type=int, default=9, help="Number of aspect ratios")
+    args = ap.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+    km = KMeanAlg(csv_file=args.annotation_file, label_dir=args.labels_dir)
+    boxes = km.get_kmean(K=args.k_ratios)
