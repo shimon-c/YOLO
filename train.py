5fdfb7309391bef7c77d3036f4945ae660e3a9ea
shimon-home2021 <82209573+shimon-home2021@users.noreply.github.com>
2025-02-02 10:14:52 +0200

    added yaml ability to set the csv names.

--- train.py
+++ train.py
@@ -74,7 +74,8 @@ def main(debug_flag=True):
     scaler = torch.cuda.amp.GradScaler()
 
     train_loader, test_loader, train_eval_loader = get_loaders(
-        train_csv_path=config.DATASET + "/train.csv", test_csv_path=config.DATASET + "/test.csv"
+        train_csv_path=config.DATASET + "/" + config.TRAIN_CSV,
+        test_csv_path=config.DATASET + "/" + config.TEST_CSV
     )
 
     if config.LOAD_MODEL:
